f2ec06e6423c0f25f283d2e6cc2fa5ce4d33742c
Victor Parpoil <vparpoil@gmail.com>
2017-06-20 17:28:18 +0200

    Adding documentation + updating package.js

--- README.md
+++ README.md
@@ -6,7 +6,39 @@ A meteor package for LinkedIn's login service.
 Install
 -----------
 ```
-meteor add pauli:accounts-linkedin
+meteor add yellowsquare:accounts-linkedin
+```
+
+Configure
+-----------
+```
+meteor add service-configuration
+```
+In your imports/startup/server/index.js file:
+```javascript
+var oauth = Meteor.settings.private.oauth;
+ServiceConfiguration.configurations.upsert(
+    {service: oauth.serviceName},
+    {
+        $set: {
+            clientId: oauth.clientId,
+            loginStyle: oauth.loginStyle,
+            secret: oauth.secret
+        }
+    }
+);
+```
+In your configuration (settings.json) :
+```json
+"private" : {
+    "oauth" :
+        {
+            "serviceName" : "linkedin",
+            "clientId"    : "",
+            "secret"      : "",
+            "loginStyle"  : "popup"
+        }
+}
 ```
 
 License
--- package.js
+++ package.js
@@ -1,25 +1,25 @@
 Package.describe({
-  name: 'pauli:accounts-linkedin',
-  summary: "Login service for LinkedIn accounts",
-  version: "2.1.2",
-  git: "https://github.com/PoBuchi/meteor-accounts-linkedin.git"
+    name: 'yellowsquare:accounts-linkedin',
+    summary: "Login service for LinkedIn accounts",
+    version: "2.1.3",
+    git: "https://github.com/yellowsquaresas/meteor-accounts-linkedin.git"
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.4.3.1')
+Package.onUse(function (api) {
+    api.versionsFrom('1.4.3.1')
 
-  api.use('accounts-base', ['client', 'server']);
-  api.imply('accounts-base', ['client', 'server']);
+    api.use('accounts-base', ['client', 'server']);
+    api.imply('accounts-base', ['client', 'server']);
 
-  api.use('accounts-oauth', ['client', 'server']);
-  api.use('pauli:linkedin-oauth@1.1.0', ['client', 'server']);
+    api.use('accounts-oauth', ['client', 'server']);
+    api.use('pauli:linkedin-oauth@1.1.0', ['client', 'server']);
 
-  api.use('pauli:linkedin-oauth');
-  api.imply('pauli:linkedin-oauth');
+    api.use('pauli:linkedin-oauth');
+    api.imply('pauli:linkedin-oauth');
 
-  // If users use accounts-ui but not facebook-config-ui, give them a tip.
-  api.use(['accounts-ui', 'pauli:linkedin-config-ui@1.1.0'], ['client', 'server'], { weak: true });
-  api.addFiles("notice.js");
+    // If users use accounts-ui but not facebook-config-ui, give them a tip.
+    api.use(['accounts-ui', 'pauli:linkedin-config-ui@1.1.0'], ['client', 'server'], {weak: true});
+    api.addFiles("notice.js");
 
-  api.addFiles("linkedin.js");
+    api.addFiles("linkedin.js");
 });
